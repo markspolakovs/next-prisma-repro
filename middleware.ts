1b444d0617fcdc2a109130390a923ddb15c4174c
Jan Piotrowski <piotrowski+github@gmail.com>
2024-03-12 19:09:06 +0000

    update Prisma to 5.11

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { Pool } from '@neondatabase/serverless'
+
 import { NextRequest, NextResponse } from "next/server";
 
-const prisma = new PrismaClient();
-
 export async function middleware(req: NextRequest): Promise<NextResponse> {
+  const neon = new Pool({ connectionString: process.env.DATABASE_URL })
+  const adapter = new PrismaNeon(neon)
+  const prisma = new PrismaClient({ adapter })
   const user = await prisma.user.findFirst();
   return NextResponse.next();
 }
